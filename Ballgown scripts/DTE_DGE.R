d9509f2aec1b41aff10a7ed34aa2b159e00e979f
Ahmed Elbashir <ahmed.a.bashir@gmail.com>
2018-04-05 11:02:41 +0200

    Now works with the three instances of data: raw, partially trimmed, and fully trimmed

--- Ballgown scripts/DTE_DGE.R
+++ Ballgown scripts/DTE_DGE.R
@@ -1,43 +1,71 @@
 
-load('bg.rda')
+execute_dge_analysis <- function(bg) {
+  ## DGE analysis
+  # Stat test
+  genes_test_results = stattest(bg, feature='gene', meas='FPKM', covariate='group')
+  
+  # Filter for results with qval less than 0.05
+  diff_genes_results = genes_test_results[!is.nan(genes_test_results$qval) & genes_test_results$qval < 0.05,]
+  
+  # Genes IDs and Genes Names
+  diff_genes_results_w_names = diff_genes_results
+  diff_genes_results_w_names$geneName = geneNames(bg)[rownames(diff_genes_results)]
+  
+  return(diff_genes_results_w_names)
+}
+
+
+execute_dte_analysis <- function(bg) {
+  ## DTE analysis
+  # Stat test
+  # Without libadjust=FALSE, no result gets past the 0.05 threshold
+  trans_test_results = stattest(bg, feature='transcript', meas='FPKM', covariate='group', libadjust=FALSE)
+  
+  # Filter for results with qval less than 0.05
+  diff_trans_results = trans_test_results[!is.nan(trans_test_results$qval) & trans_test_results$qval < 0.05,]
+  
+  # Join differentiated transcripts and gene IDs and Names
+  
+  transcript_gene_table = indexes(bg)$t2g
+  
+  # Dataframe containing transcripts IDs, genes IDs, and genes Names
+  trans_gens_names = transcript_gene_table
+  trans_gens_names$transcriptName = transcriptNames(bg)
+  trans_gens_names$geneName = geneNames(bg)
+  
+  diff_trans_results_w_genes_names = merge(x = diff_trans_results, y = trans_gens_names, by.x = "id", by.y = "t_id")
+  
+  return(diff_trans_results_w_genes_names)
+}
+
 
 library(ballgown)
+
+## Load data
+## ===========
+raw_bg = readRDS('raw_bg.rd')
+partial_bg = readRDS('partial_bg.rd')
+full_bg = readRDS('full_bg.rd')
+
+
 ## DTE analysis
+## ===========
+raw_dte_results = execute_dte_analysis(raw_bg)
+partial_dte_results = execute_dte_analysis(partial_bg)
+full_dte_results = execute_dte_analysis(full_bg)
 
-# Stat test
-# Without libadjust=FALSE, no result gets past the 0.05 threshold
-trans_test_results = stattest(bg, feature='transcript', meas='FPKM', covariate='group', libadjust=FALSE)
+View(head(raw_dte_results))
+View(head(partial_dte_results))
+View(head(full_dte_results))
+#nrow(diff_trans_results_w_genes_names)
 
-# Filter for results with qval less than 0.05
-diff_trans_results = trans_test_results[!is.nan(trans_test_results$qval) & trans_test_results$qval < 0.05,]
-
-# Join differentiated transcripts and gene IDs and Names
-
-transcript_gene_table = indexes(bg)$t2g
-
-# Dataframe containing transcripts IDs, genes IDs, and genes Names
-trans_gens_names = transcript_gene_table
-trans_gens_names$transcriptName = transcriptNames(bg)
-trans_gens_names$geneName = geneNames(bg)
-
-diff_trans_results_w_genes_names = merge(x = diff_trans_results, y = trans_gens_names, by.x = "id", by.y = "t_id")
-
-View(head(diff_trans_results_w_genes_names))
-nrow(diff_trans_results_w_genes_names)
 
 ## DGE analysis
+## ===========
+raw_dge_results = execute_dge_analysis(raw_bg)
+partial_dge_results = execute_dge_analysis(partial_bg)
+full_dge_results = execute_dge_analysis(full_bg)
 
-# Stat test
-genes_test_results = stattest(bg, feature='gene', meas='FPKM', covariate='group')
-
-# Filter for results with qval less than 0.05
-diff_genes_results = genes_test_results[!is.nan(genes_test_results$qval) & genes_test_results$qval < 0.05,]
-
-# Genes IDs and Genes Names
-diff_genes_results_w_names = diff_genes_results
-diff_genes_results_w_names$geneName = geneNames(bg)[rownames(diff_genes_results)]
-
-View(diff_genes_results_w_names)
-
-
-
+View(head(raw_dge_results))
+View(head(partial_dge_results))
+View(head(full_dge_results))
--- Ballgown scripts/load_data.R
+++ Ballgown scripts/load_data.R
@@ -1,14 +1,16 @@
 
-data_directory = 'C:/Users/Admin/Documents/H3ABioNet/rna_practice/tm_output/'
-
-sample37 <- paste(data_directory, '37', sep = '')
-sample38 <- paste(data_directory, '38', sep = '')
-sample39 <- paste(data_directory, '39', sep = '')
-sample40 <- paste(data_directory, '40', sep = '')
-sample41 <- paste(data_directory, '41', sep = '')
-sample42 <- paste(data_directory, '42', sep = '')
 
 library(ballgown)
-bg = ballgown(samples=c(sample37, sample38, sample39, sample40, sample41, sample42), meas='all')
-pData(bg) = data.frame(id=sampleNames(bg), group=rep(c(1,0), each=3))
-save(bg, file='bg.rda')
+
+root_path = 'C:/Users/Admin/Documents/H3ABioNet/rna_assessment/stringtie-output/'
+
+for (data_dir in c('raw', 'partial', 'full')) {
+  data_path = paste(root_path, data_dir, sep = '')
+  samples_paths = c()
+  for (i in 1:6) {
+    samples_paths[i] = paste(data_path, '/sample', i, sep="")
+  }
+  bg = ballgown(samples=samples_paths, meas='all')
+  pData(bg) = data.frame(id=sampleNames(bg), group=rep(c(1,0), each=3))
+  saveRDS(bg, file=paste(data_dir, '_bg.rd', sep = ''))
+}
